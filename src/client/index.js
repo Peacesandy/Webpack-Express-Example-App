b6443c6d4dee92ffd1c13737549124150bf6c1e4
Peace Sandy <peacesandy04@gmail.com>
2024-09-08 14:59:48 +0100

    added plugins

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,10 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import { functionName } from './js';
 
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I EXIST");
+
+
+export { functionName };
